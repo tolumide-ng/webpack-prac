36194400012f267bb752e60d2315547f7839555f
tolumide-ng <tolumideshopein@gmail.com>
2019-07-13 12:14:50 +0100

    Add first loaders (css/style)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { ComponentService } from "./app/component.service";
 const alertService = new AlertService();
 const componentService = new ComponentService();
 
+import "./main.css";
+
 import { run } from "./app/app";
 
 run(alertService, componentService);
