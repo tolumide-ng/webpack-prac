afeeeefd065f356570b3c32eb48cafedf510fdc3
tolumide-ng <tolumideshopein@gmail.com>
2019-07-13 11:13:23 +0100

    Webpack now bundling all our code

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { AlertService } from "./app/alert.service";
+import { ComponentService } from "./app/component.service";
+const alertService = new AlertService();
+const componentService = new ComponentService();
+
+import { run } from "./app/app";
+
+run(alertService, componentService);
